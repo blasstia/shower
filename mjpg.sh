39c221dc87f48710653748a3892d5a79e9ad76b8
nieh <blasstia@gmail.com>
2021-10-11 10:28:52 +0800

    Revert "main"
    This reverts commit 3261c19b264e719b8fca6645ef473e938bc5aaf3.

--- mjpg.sh
+++
@@ -1,6 +0,0 @@
-export STREAMER_PATH=/home/pi/code/mjpg-streamer/
-export LD_LIBRARY_PATH=$STREAMER_PATH
-$STREAMER_PATH/mjpg_streamer -i "input_uvc.so -y -n" -o "output_http.so -w $STREAMER_PATH/www"
-
-
-
