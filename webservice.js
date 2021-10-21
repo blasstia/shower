3ce1db33d9441c1b2926975a82f6181ea8efec5e
nieh <blasstia@gmail.com>
2021-10-21 10:04:28 +0800

    Merge branch 'main' of https://github.com/blasstia/shower into main

---
+++ webservice.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+const server = http.createServer((req, res) => {
+  res.StatusCode = 200;
+  res.setHeader('Content-Type','text/html');
+  res.end('<b>Hello World</b>');
+});
+
+server.listen(port, hostname, () => {
+  console.log('Server running at http://127.0.0.1:3000/');
+});
